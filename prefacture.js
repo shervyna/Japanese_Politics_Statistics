70d6eab95aa417ccc040a2a60a481daa72aebae5
shervyna <shervyna715@gmail.com>
2017-01-06 23:47:38 -0800

    basic content with interactive map and sidebar

---
+++ prefacture.js
@@ -0,0 +1,79 @@
+var prefacture=[
+{
+	id : 1,
+	name: 'Hokkaido',
+}
+
+{
+	id : 2,
+	name : 'Aomori',
+}
+{
+	id: 3,
+	name : 'Iwate',
+}
+];
+/*
+4. Miyagi
+5. Akita
+6. Yamagata
+7. Fukushima
+
+Kant?
+
+8. Ibaraki
+9. Tochigi
+10. Gunma
+11. Saitama
+12. Chiba
+13. Tokyo
+14. Kanagawa
+
+Ch?bu
+
+15. Niigata
+16. Toyama
+17. Ishikawa
+18. Fukui
+19. Yamanashi
+20. Nagano
+21. Gifu
+22. Shizuoka
+23. Aichi
+
+Kansai
+
+24. Mie
+25. Shiga
+26. Kyoto
+27. Osaka
+28. Hy?go
+29. Nara
+30. Wakayama
+
+Ch?goku
+
+31. Tottori
+32. Shimane
+33. Okayama
+34. Hiroshima
+35. Yamaguchi
+
+Shikoku
+
+36. Tokushima
+37. Kagawa
+38. Ehime
+39. K?chi
+
+Ky?sh?
+
+40. Fukuoka
+41. Saga
+42. Nagasaki
+43. Kumamoto
+44. ?ita
+45. Miyazaki
+46. Kagoshima
+47. Okinawa
+];*/
---
+++ script.js
@@ -0,0 +1,9 @@
+var tooltipSpan = document.getElementsByClassName('tooltiptext');
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan[0].style.top = (y - 160) + 'px';
+    tooltipSpan[0].style.left = (x - 600) + 'px';
+};
+
