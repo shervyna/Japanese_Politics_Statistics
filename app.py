010f11993d09580bbb2808f9c14e2c9bfaa33f29
shervyna <shervyna715@gmail.com>
2017-01-07 21:08:20 -0800

    change host

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,request,render_template
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ def index():
    return render_template('japan_web.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+  # app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
