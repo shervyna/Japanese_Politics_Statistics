89b73342f590c5ec2c693ebb317f1ee305005baf
shervyna <shervyna715@gmail.com>
2017-01-07 21:24:17 -0800

    change tooltip x axis

--- static/script.js
+++ static/script.js
@@ -4,6 +4,6 @@ window.onmousemove = function (e) {
     var x = e.clientX,
         y = e.clientY;
     tooltipSpan[0].style.top = (y - 160) + 'px';
-    tooltipSpan[0].style.left = (x - 600) + 'px';
+    tooltipSpan[0].style.left = (x - 500) + 'px';
 };
 
