a79210f11e9a1fec595b4789d7de6a7d324677f1
shervyna ruan <hsuanwei.ruan@yale.edu>
2017-01-08 09:36:39 +0000

    scale svg map and relevant fixes

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
 var tooltipSpan = document.getElementsByClassName('tooltiptext');
 
 window.onmousemove = function (e) {
-    var x = e.clientX,
-        y = e.clientY;
-    tooltipSpan[0].style.top = (y - 160) + 'px';
-    tooltipSpan[0].style.left = (x - 500) + 'px';
+    var x = e.pageX,
+        y = e.pageY;
+    tooltipSpan[0].style.top = (y-50) + 'px';
+    tooltipSpan[0].style.left = (x-600) + 'px';
 };
 
